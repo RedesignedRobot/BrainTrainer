f0a4ab963a3fbaa45a21e4c4070be848837f3070
Amir Ayub <dev.amirayub@gmail.com>
2016-10-28 23:09:16 +0400

    Object declarations done

--- app/src/main/java/com/aaxs/nitrov17/braintrainer/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/braintrainer/MainActivity.java
@@ -1,14 +1,19 @@
 package com.aaxs.nitrov17.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView timerText,eqText,scoreText;
     Button choice1,choice2,choice3,choice4;
+    CountDownTimer countDownTimer;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
