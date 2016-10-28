69e772b19f23effd67efdd2a435fe8dc0422fed8
Amir Ayub <dev.amirayub@gmail.com>
2016-10-28 23:07:42 +0400

    Wired app frame

--- app/src/main/java/com/aaxs/nitrov17/braintrainer/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/braintrainer/MainActivity.java
@@ -2,8 +2,13 @@ package com.aaxs.nitrov17.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timerText,eqText,scoreText;
+    Button choice1,choice2,choice3,choice4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
